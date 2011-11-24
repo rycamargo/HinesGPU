83f71110fd83277ab17c10297269404540151b3e
rycamargo <rycamargo@gmail.com>
2011-11-24 15:34:16 -0200

    extra buildFiles

--- Hines/src/buildLinuxMpi.sh
+++ Hines/src/buildLinuxMpi.sh
@@ -4,7 +4,7 @@ MPI_LINK_FLAGS="-L/usr/lib -lmpich -lopa -lmpl -lrt -lcr -lpthread"
 #$(echo $(mpicc --showme:link))
         
 g++ -c -O3  ${MPI_COMPILE_FLAGS} SynapticChannels.cpp launchHines.cpp ActiveChannels.cpp HinesMatrix.cpp PlatformFunctions.cpp SynapticChannels.cpp Connections.cpp SpikeStatistics.cpp NeuronInfoWriter.cpp PerformSimulation.cpp CpuSimulationControl.cpp
-nvcc $1 ${MPI_COMPILE_FLAGS} ${MPI_LINK_FLAGS} --compiler-options -O3 -maxrregcount=124 --ptxas-options=-v -m64 -arch sm_13 -o HinesGpu ActiveChannels.o HinesMatrix.o PlatformFunctions.o SynapticChannels.o Connections.o SpikeStatistics.o launchHines.o NeuronInfoWriter.o PerformSimulation.o CpuSimulationControl.o HinesGpu.cu PrepareGpuExecution.cu SynapticComm.cu
+nvcc $1 ${MPI_COMPILE_FLAGS} ${MPI_LINK_FLAGS} --compiler-options -O3 -maxrregcount=124 --ptxas-options=-v -m64 -arch sm_13 -o HinesGpu ActiveChannels.o HinesMatrix.o PlatformFunctions.o SynapticChannels.o Connections.o SpikeStatistics.o launchHines.o NeuronInfoWriter.o PerformSimulation.o CpuSimulationControl.o HinesGpu.cu GpuSimulationControl.cu SynapticComm.cu
 
 #OpenMPI 1.2
 #g++ -c -D_REENTRANT -O3 SynapticChannels.cpp launchHines.cpp ActiveChannels.cpp HinesMatrix.cpp PlatformFunctions.cpp SynapticChannels.cpp Connections.cpp SpikeStatistics.cpp
--- Hines/src/buildMac.sh
+++ Hines/src/buildMac.sh
@@ -1,5 +1,5 @@
 g++ -c -O3 SynapticChannels.cpp launchHines.cpp ActiveChannels.cpp HinesMatrix.cpp PlatformFunctions.cpp SynapticChannels.cpp Connections.cpp SpikeStatistics.cpp
-nvcc $1 --compiler-options -O3 -maxrregcount=124 --ptxas-options=-v -m32 -arch sm_11 -o HinesGpu HinesGpu.cu PrepareGpuExecution.cu SynapticComm.cu launchHines.o ActiveChannels.o HinesMatrix.o PlatformFunctions.o SynapticChannels.o Connections.o SpikeStatistics.o
+nvcc $1 --compiler-options -O3 -maxrregcount=124 --ptxas-options=-v -m32 -arch sm_11 -o HinesGpu HinesGpu.cu GpuSimulationControl.cu SynapticComm.cu launchHines.o ActiveChannels.o HinesMatrix.o PlatformFunctions.o SynapticChannels.o Connections.o SpikeStatistics.o
 
 #nvcc $1 -D_REENTRANT -I/usr//include --compiler-options -O3 -maxrregcount=124 --ptxas-options=-v -arch sm_13 -o HinesGpu HinesGpu.cu PrepareGpuExecution.cu SynapticComm.cu launchHines.o ActiveChannels.o HinesMatrix.o PlatformFunctions.o SynapticChannels.o Connections.o SpikeStatistics.o
 #g++ -std=c++0x -c -O3 SynapticChannels.cpp launchHines.cpp ActiveChannels.cpp HinesMatrix.cpp PlatformFunctions.cpp SynapticChannels.cpp Connections.cpp SpikeStatistics.cpp
